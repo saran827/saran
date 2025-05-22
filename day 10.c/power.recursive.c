9aba103715de059db79d908f47ca4fabdf618b95
saran827 <ssaran65078@gmail.com>
2025-05-22 15:49:26 +0530

    day 6.c

---
+++ day 10.c/power.recursive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int power(int a,int b)
+{
+    if(b==1)
+    {
+        return a;
+    }
+        return a*power(a,b-1);
+}
+int main()
+{
+    printf("%d",power(2,3));
+    return 0;
+    
+}
